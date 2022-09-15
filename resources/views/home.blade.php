038212ecdb2f63fb41656276c32616efc2f8cde0
Steveh77 <stederosa76@gmail.com>
2022-09-16 01:32:45 +0200

    Added cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,19 @@
 @extends('layout.main')
 
 @section('home-content')
-    <div class="card-container">
-        <div class="card">
-            <img src="../../public/images/" alt="">
+<section id="series">
+    <div class="container">
+        <h1>CURRENT SERIES</h1>
+        <div class="card-container"> 
+            @foreach ($comics as $item)
+            <div class="card">
+                <img src="{{$item['thumb']}}" alt="">
+                <h4>{{$item['series']}}</h4>
+            </div>
+            @endforeach
         </div>
+        <h3>Load more</h3>
+    
     </div>
+</section>
 @endsection
